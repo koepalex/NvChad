82fda634c77d2a766955a44b500f26aae0f2cc84
Alexander Köpke <alexander.koepke@microsoft.com>
2024-07-11 20:58:47 +0000

    fix tab select for suggestions

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -44,12 +44,6 @@ local function border(hl_name)
   }
 end
 
-local has_words_before = function()
-  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
-  local line, col = vim.api.nvim_win_get_cursor(0)
-  return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
-end
-
 local options = {
   completion = {
     completeopt = "menu,menuone",
@@ -86,9 +80,7 @@ local options = {
       select = true,
     },
     ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() and has_words_before() then
-        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-      elseif cmp.visible() then
+      if cmp.visible() then
         cmp.select_next_item()
       elseif require("luasnip").expand_or_jumpable() then
         vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
